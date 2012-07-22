8a06bed910e153f4b22d12b24ced0839209425af
Emmanuel <emmanuel@MacBook-Emmanuel.local>
2012-07-22 21:51:41 +0200

    Initial commit

---
+++ src/main/python/Grid.py
@@ -0,0 +1,5 @@
+class Grid():
+    def set(self, x, y, val):
+        pass
+    def display(self):
+        return 'a'
---
+++ src/test/python/GridTest.py
@@ -0,0 +1,13 @@
+from Grid import Grid
+
+class GridTest():
+    def test_display(self):
+        grid = Grid()
+        for x in range(9):
+            for y in range(9):
+                if x==y:
+                    grid.set(x, y, x)
+        assert "1........\n.2.......\n..3......\n...4.....\n....5....\n.....6...\n......7..\n.......8.\n........9\n" == grid.display()
+
+if __name__ == '__main__':
+    unittest.main()
