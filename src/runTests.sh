f322e8999776b5005d13501c89fc7f26b266e3dc
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-07-23 14:28:24 +0200

    Delete of useless stuff in runTests.sh

--- src/runTests.sh
+++ src/runTests.sh
@@ -3,9 +3,6 @@
 REPERTOIRE_SCRIPT=`dirname $0`
 REPERTOIRE_TEST="$REPERTOIRE_SCRIPT/test/python"
 export PYTHONPATH="$REPERTOIRE_SCRIPT/main/python/"
-ROUGE="[31;1m"
-VERT="[32;1m"
-JAUNE="[33;1m"
 
 find -name "*.pyc" -exec rm {} \;
          
