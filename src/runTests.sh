08e89551a138349ab6520752f73f7904ab643f1e
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-07-23 23:30:30 +0200

    Rendered runTests.sh compatible with Mac bash

--- src/runTests.sh
+++ src/runTests.sh
@@ -4,6 +4,6 @@ REPERTOIRE_SCRIPT=`dirname $0`
 REPERTOIRE_TEST="$REPERTOIRE_SCRIPT/test/python"
 export PYTHONPATH="$REPERTOIRE_SCRIPT/main/python/"
 
-find -name "*.pyc" -exec rm {} \;
+find . -name "*.pyc" -exec rm {} \;
          
 nosetests $REPERTOIRE_TEST
