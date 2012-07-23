ff67fa00314251bacd58032641f308a36398a23b
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-07-23 16:25:47 +0200

    Added Grid comments

--- src/main/python/Grid.py
+++ src/main/python/Grid.py
@@ -1,12 +1,32 @@
+SIZE = 9
+
 class Grid():
-    candidates = [['.' for x in range(9)] for y in range(9)]
+    """ The sudoku grid of 9x9 cells.
+        Each cell features a list of the 1 to 9 remaining candidates (from numbers 1 to 9) allowed for this cell.
+        When a cell has only one remaining candidate (list of size 1), it is the definitive number found for this cell.
+    """
+    # initialization of the grid, with each of the 9x9 cells containing the list [1,2,3,4,5,6,7,8,9] of all possible values
+    candidates = [[range(1, 10) for x in range(SIZE)] for y in range(SIZE)]
 
     def set(self, x, y, val):
-        self.candidates[x][y] = str(val)
+        """ Sets a cell to one number, ie a list containing one element : the specified value.
+            Will serve for setting the initial numbers given with the grid.
+        """
+        self.candidates[x][y] = [val]
 
     def display(self):
-        #print self.candidates 
+        """ Returns a string representation of the grid in the current state.
+            Cells whose value has been found are displayed by the corresponding number (from 1 to 9).
+            Cells with two or more remaining possible values are displayed with a dot (".").
+            Each row ends with a line return ("\n").
+        """
         grid = ""
-        for y in range(9):
-            grid = grid + "".join(self.candidates[y]) + "\n"
+        for y in range(SIZE):
+            for x in range(SIZE):
+                cell_candidates = self.candidates[x][y]
+                if len(cell_candidates) > 1:
+                    grid = grid + "."
+                else:
+                    grid = grid + str(cell_candidates[0])
+            grid = grid + "\n"
         return grid
