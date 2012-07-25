c7adfa7fb69f0c4f78f4edd5ad5a45bc362a543b
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-07-25 16:11:25 +0200

    ObviousResolution vertical_obvious() and block_obvious()

--- src/main/python/ObviousResolution.py
+++ src/main/python/ObviousResolution.py
@@ -4,13 +4,12 @@
 from Grid import SIZE
 
 class ObviousResolution:
-    """ Very obvious ways of finding the value of a cell.
+    """ Most obvious ways of finding the value of a cell.
         If a line, column or 3x3 block contains all numbers but one, the empty cell can only be that remaining number
     """
-    def horizontal_singleton(self, grid, x, y):
+    def horizontal_obvious(self, grid, x, y):
         """ Check if the whole line except the (x, y) cell has been found.
-            In that case, the number of that cell is trivial.
-            Returns 1 if the cell's number has been determined, 0 otherwise.
+            Returns 1 and set the number of that cell in that case, returns 0 otherwise.
         """
         for i in range(SIZE):
             if i == x:
@@ -20,3 +19,34 @@ class ObviousResolution:
             else:
                 grid.remove_candidate(x, y, grid.candidates[i][y][0])
         return 1
+
+    def vertical_obvious(self, grid, x, y):
+        """ Check if the whole column except the (x, y) cell has been found.
+            Returns 1 and set the number of that cell in that case, returns 0 otherwise.
+        """
+        for j in range(SIZE):
+            if j == y:
+                continue
+            if len(grid.candidates[x][j]) > 1:
+                return 0
+            else:
+                grid.remove_candidate(x, y, grid.candidates[x][j][0])
+        return 1
+
+    def block_obvious(self, grid, x, y):
+        """ Check if the whole 3x3 block except the (x, y) cell has been found.
+            Returns 1 and set the number of that cell in that case, returns 0 otherwise.
+        """
+        xStartOfBlock = (x / 3) * 3
+        yStartOfBlock = (y / 3) * 3
+        for i in range(3):
+            for j in range(3):
+                if xStartOfBlock + i == x and yStartOfBlock + j == y:
+                    continue
+                candidatesForTheCell = grid.candidates[xStartOfBlock + i][yStartOfBlock + j]
+                if len(candidatesForTheCell) > 1:
+                    return 0
+                else:
+                    grid.remove_candidate(x, y, candidatesForTheCell[0])
+        return 1
+
--- src/test/python/TestObviousResolution.py
+++ src/test/python/TestObviousResolution.py
@@ -10,11 +10,23 @@ class TestObviousResolution(unittest.TestCase):
     def setUp(self):
         self.obviousResolution = ObviousResolution()
 
-    def test_horizontal_singleton(self):
+    def test_horizontal_obvious(self):
         grid = Grid(StringIO(".........\n.23456789\n.........\n.234.6789\n.........\n.........\n.........\n.........\n.........\n"))
-        self.assertEqual(1, self.obviousResolution.horizontal_singleton(grid, 0, 1))
-        self.assertEqual(0, self.obviousResolution.horizontal_singleton(grid, 0, 3))
+        self.assertEqual(1, self.obviousResolution.horizontal_obvious(grid, 0, 1))
+        self.assertEqual(0, self.obviousResolution.horizontal_obvious(grid, 0, 3))
         self.assertEqual(".........\n123456789\n.........\n.234.6789\n.........\n.........\n.........\n.........\n.........\n", grid.display())
+
+    def test_vertical_obvious(self):
+        grid = Grid(StringIO("...1..9..\n...2..8..\n...3.....\n......6..\n...5..5..\n...6..4..\n......3..\n...8..2..\n...9..1..\n"))
+        self.assertEqual(1, self.obviousResolution.vertical_obvious(grid, 6, 2))
+        self.assertEqual(0, self.obviousResolution.vertical_obvious(grid, 3, 3))
+        self.assertEqual("...1..9..\n...2..8..\n...3..7..\n......6..\n...5..5..\n...6..4..\n......3..\n...8..2..\n...9..1..\n", grid.display())
+
+    def test_block_obvious(self):
+        grid = Grid(StringIO("...123...\n...456...\n...78....\n1.3......\n.56......\n789......\n.........\n.........\n.........\n"))
+        self.assertEqual(1, self.obviousResolution.block_obvious(grid, 5, 2))
+        self.assertEqual(0, self.obviousResolution.block_obvious(grid, 1, 3))
+        self.assertEqual("...123...\n...456...\n...789...\n1.3......\n.56......\n789......\n.........\n.........\n.........\n", grid.display())
 
 if __name__ == '__main__':
     unittest.main()
