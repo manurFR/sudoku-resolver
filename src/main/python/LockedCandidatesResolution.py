559eebda819db7b2bfad4360bb297928175077fb
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-09-11 18:10:04 +0200

    LockedCandidatesResolution block_column_reduction()

--- src/main/python/LockedCandidatesResolution.py
+++ src/main/python/LockedCandidatesResolution.py
@@ -7,23 +7,23 @@ from Grid import SIZE
 
 class LockedCandidatesResolution:
     """ Locked candidates (also known as Pointed Pairs or Pointed Triples) come in two types, almost similar but actually different :
-        - Type 1 "Line->Box reduction" : two or three aligned cells inside a block share a same remaining candidate, and that digit is not a remaining 
-          candidate in any other cell OF THE BLOCK. Since a block must feature each digit, that candidate must appear on this alignment of two or three 
+        - Type 1 "Line->Box reduction" : two or three aligned cells inside a block share a same remaining candidate, and that digit is not a remaining
+          candidate in any other cell OF THE BLOCK. Since a block must feature each digit, that candidate must appear on this alignment of two or three
           cells. Consequently, it can not appear on any other cell of the same LINE (row or column). The corresponding candidates can be removed.
         - Type 2 "Box->Line reduction" : two or three aligned cells inside a block share a same remaining candidate, and that digit is not a remaining
-          candidate in any other cell OF THE SAME LINE (row or column). Since a line must feature each digit, that candidate must appear in one of these 
+          candidate in any other cell OF THE SAME LINE (row or column). Since a line must feature each digit, that candidate must appear in one of these
           two or three cells. Consequently, it can not appear on any other cell of the same BLOCK. The corresponding candidates can be removed.
-        This resolution will only infrequently lead to solving cells, but it may remove remaining candidates that otherwise prevent other resolution methods to succeed.
+        These resolutions will only infrequently lead to solving cells, but it may remove remaining candidates that otherwise prevent other resolution methods to succeed.
     """
     def run(self, grid):
         solvedCells = 0
         for xBlock in [0, 3, 6]:
             for yBlock in [0, 3, 6]:
-                solvedCells += self.horizontal_line_box_reduction(grid, xBlock, yBlock) + self.vertical_line_box_reduction(grid, xBlock, yBlock)
+                solvedCells += self.row_block_reduction(grid, xBlock, yBlock) + self.column_block_reduction(grid, xBlock, yBlock) + self.block_column_reduction(grid, xBlock, yBlock)
         Stats.increment(self.__class__.__name__, solvedCells)
         return solvedCells
 
-    def horizontal_line_box_reduction(self, grid, xBlock, yBlock):
+    def row_block_reduction(self, grid, xBlock, yBlock):
         """ For each of the 9 digits not already solved in the (xBlock, yBlock) block, check if this digit is included in the remaining candidates
              of two or three cells in a row, AND not included in the remaining candidates of the other two rows.
             If it is the case, remove this digit from the remaining candidates of all the cells in the same row outside of the block.
@@ -49,16 +49,15 @@ class LockedCandidatesResolution:
                 rowToProcess = rowsWhereThisDigitIsPresentTwiceOrThrice.pop()
                 for i in range(SIZE):
                     if (i < xBlock or i > xBlock + 2) and d in grid.candidates[i][rowToProcess]:
-                        logging.debug("Locked Candidates resolution (horizontal) : removing candidate {} in ({},{}) thanks to locked candidates in block ({},{})".format(d, i, rowToProcess, xBlock, yBlock))
+                        logging.debug("Locked Candidates resolution (row_block) : removing candidate {} in ({},{}) thanks to locked candidates in block ({},{})".format(d, i, rowToProcess, xBlock, yBlock))
                         grid.remove_candidate(i, rowToProcess, d)
                         if grid.get_solution(i, rowToProcess) != None:
                             logging.info("Locked Candidates resolution : found value for ({},{}) : {}".format(i, rowToProcess, grid.get_solution(i, rowToProcess)))
                             solvedCells += 1
         return solvedCells
 
-    def vertical_line_box_reduction(self, grid, xBlock, yBlock):
+    def column_block_reduction(self, grid, xBlock, yBlock):
         """ For each of the 9 digits not already solved in the (xBlock, yBlock) block, check if this digit is included in the remaining candidates
-
              of two or three cells in a column, AND not included in the remaining candidates of the other two columns.
             If it is the case, remove this digit from the remaining candidates of all the cells in the same column outside of the block.
             Returns the number of cells solved by this process.
@@ -83,11 +82,46 @@ class LockedCandidatesResolution:
                 colToProcess = colsWhereThisDigitIsPresentTwiceOrThrice.pop()
                 for i in range(SIZE):
                     if (i < yBlock or i > yBlock + 2) and d in grid.candidates[colToProcess][i]:
-                        logging.debug("Locked Candidates resolution (vertical) : removing candidate {} in ({},{}) thanks to locked candidates in block ({},{})".format(d, colToProcess, i,  xBlock, yBlock))
+                        logging.debug("Locked Candidates resolution (column->block) : removing candidate {} in ({},{}) thanks to locked candidates in block ({},{})".format(d, colToProcess, i,  xBlock, yBlock))
                         grid.remove_candidate(colToProcess, i, d)
                         if grid.get_solution(colToProcess, i) != None:
                             logging.info("Locked Candidates resolution : found value for ({},{}) : {}".format(colToProcess, i, grid.get_solution(colToProcess, i)))
                             solvedCells += 1
+        return solvedCells
+
+    def block_column_reduction(self, grid, xBlock, yBlock):
+        """ For each of the 9 digits not already solved in the (xBlock, yBlock) block, check if this digit is included in the remaining candidates
+             of two or three cells in a column, AND not included in the remaining candidates of the rest of the column.
+            If it is the case, remove this digit from the remaining candidates of all the cells in the block outside of the column.
+            Returns the number of cells solved by this process.
+        """
+        digits = self.remaining_candidates_in_block(grid, xBlock, yBlock)
+        if not digits:
+            return 0
+
+        solvedCells = 0
+        for d in digits:
+            for col in range(xBlock, xBlock+3):
+                occurrencesInTheBlock = 0
+                occurrencesOutsideTheBlock = 0
+                for row in range(SIZE):
+                    if d in grid.candidates[col][row]:
+                        if row >= yBlock and row <= yBlock+2:
+                            occurrencesInTheBlock += 1
+                        else:
+                            occurrencesOutsideTheBlock += 1
+
+                if occurrencesInTheBlock >= 2 and occurrencesOutsideTheBlock == 0: # if there are 2 or 3 cells in the block featuring the current digit as remaining candidate and no other cell in the same column...
+                    for x in range(xBlock, xBlock+3):
+                        if x == col:
+                            continue # don't remove the digit from the remaining candidates in the current column
+                        for y in range(yBlock, yBlock+3):
+                            if d in grid.candidates[x][y]:
+                                logging.debug("Locked Candidates resolution (block->column) : removing candidate {} in ({},{}) thanks to locked candidates in block ({},{})".format(d, x, y, xBlock, yBlock))
+                                grid.remove_candidate(x, y, d)
+                                if grid.get_solution(x, y) != None:
+                                    logging.info("Locked Candidates resolution : found value for ({},{}) : {}".format(x, y, grid.get_solution(x, y)))
+                                    solvedCells += 1
         return solvedCells
 
     def remaining_candidates_in_block(self, grid, xBlock, yBlock):
--- src/test/python/TestLockedCandidatesResolution.py
+++ src/test/python/TestLockedCandidatesResolution.py
@@ -11,28 +11,48 @@ class TestLockedCandidatesResolution(unittest.TestCase):
     def setUp(self):
         self.lockedCandidatesResolution = LockedCandidatesResolution()
 
-    def test_horizontal_line_box_reduction(self):
+    def test_row_block_reduction(self):
         grid = Grid(StringIO(".179.36..\n....8....\n9.....5.7\n.72.1.43.\n...4.2.7.\n.6437.25.\n7.1....65\n....3....\n..56.172.\n"))
         prepareRemainingCandidates(grid)
         self.assertItemsEqual([2, 3, 4, 5, 6], grid.candidates[0][1])
-        self.assertItemsEqual([2, 3, 4, 5], grid.candidates[1][1])
-        self.assertItemsEqual([3, 6], grid.candidates[2][1])
-        self.assertEqual(1, self.lockedCandidatesResolution.horizontal_line_box_reduction(grid, 6, 0))
+        self.assertItemsEqual([2, 3, 4, 5],    grid.candidates[1][1])
+        self.assertItemsEqual([3, 6],          grid.candidates[2][1])
+        self.assertEqual(1, self.lockedCandidatesResolution.row_block_reduction(grid, 6, 0))
         self.assertItemsEqual([2, 4, 5, 6], grid.candidates[0][1])
-        self.assertItemsEqual([2, 4, 5], grid.candidates[1][1])
-        self.assertItemsEqual([6], grid.candidates[2][1])
+        self.assertItemsEqual([2, 4, 5],    grid.candidates[1][1])
+        self.assertItemsEqual([6],          grid.candidates[2][1])
         self.assertEqual(".179.36..\n..6.8....\n9.....5.7\n.72.1.43.\n...4.2.7.\n.6437.25.\n7.1....65\n....3....\n..56.172.\n", grid.display())
 
-    def test_vertical_line_box_reduction(self):
+    def test_column_block_reduction(self):
         grid = Grid(StringIO("000023400004000100050084090601070902793206801000010760000000009800000004060000587"))
         prepareRemainingCandidates(grid)
         self.assertItemsEqual([1, 3, 4, 5, 6, 7, 8], grid.candidates[3][6])
-        self.assertItemsEqual([1, 3, 5, 6, 7, 9], grid.candidates[3][7])
-        self.assertItemsEqual([1, 3, 4, 9], grid.candidates[3][8])
-        self.assertEqual(0, self.lockedCandidatesResolution.vertical_line_box_reduction(grid, 3, 0))
+        self.assertItemsEqual([1, 3, 5, 6, 7, 9],    grid.candidates[3][7])
+        self.assertItemsEqual([1, 3, 4, 9],          grid.candidates[3][8])
+        self.assertEqual(0, self.lockedCandidatesResolution.column_block_reduction(grid, 3, 0))
         self.assertItemsEqual([3, 4, 5, 6, 7, 8], grid.candidates[3][6])
-        self.assertItemsEqual([3, 5, 6, 7, 9], grid.candidates[3][7])
-        self.assertItemsEqual([3, 4, 9], grid.candidates[3][8])
+        self.assertItemsEqual([3, 5, 6, 7, 9],    grid.candidates[3][7])
+        self.assertItemsEqual([3, 4, 9],          grid.candidates[3][8])
+
+    def test_block_column_reduction(self):
+        grid = Grid(StringIO("500200010001900730000000800050020008062039000000004300000000000080467900007300000"))
+        prepareRemainingCandidates(grid)
+        self.assertItemsEqual([2, 4, 5, 6, 7, 8],    grid.candidates[7][6])
+        self.assertItemsEqual([2, 5],                grid.candidates[7][7])
+        self.assertItemsEqual([2, 4, 5, 6, 8],       grid.candidates[7][8])
+        self.assertItemsEqual([1, 2, 3, 4, 5, 6, 7], grid.candidates[8][6])
+        self.assertItemsEqual([1, 2, 3, 5],          grid.candidates[8][7])
+        self.assertItemsEqual([1, 2, 4, 5, 6],       grid.candidates[8][8])
+        self.assertEqual(1, self.lockedCandidatesResolution.block_column_reduction(grid, 6, 6))
+        self.assertItemsEqual([4, 5, 6, 7, 8],    grid.candidates[7][6])
+        self.assertItemsEqual([5],                grid.candidates[7][7])
+        self.assertItemsEqual([4, 5, 6, 8],       grid.candidates[7][8])
+        self.assertItemsEqual([1, 3, 4, 5, 6, 7], grid.candidates[8][6])
+        self.assertItemsEqual([1, 3, 5],          grid.candidates[8][7])
+        self.assertItemsEqual([1, 4, 5, 6],       grid.candidates[8][8])
+        self.assertEqual("5..2...1...19..73.......8...5..2...8.62.39........43............8.46795...73.....", grid.display(lineBreak=False))
+
+        
 
 if __name__ == '__main__':
     unittest.main()
